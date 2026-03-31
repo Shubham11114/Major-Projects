23c670e3af27a394c423a428a99e16a6fdf9784e
Shubham kumar <shubham11114@hotmail.com>
2026-03-31 18:50:02 +0530

    moved auth files to views and updated routes and navbar accordingly

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,28 @@ const LocalStrategy = require("passport-local");
 const User = require("../models/user.js");
 const path = require("path");
 
+
 router.get("/signup", (req, res) => {
-   res.send("Signup Page");
+  res.render("users/signup.ejs", { hideAuthButtons: true });
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs", { hideAuthButtons: true });
+});
+
+router.get("/forget-password", (req, res) => {
+  res.render("users/forget.ejs", { hideAuthButtons: true });
+});
+
+router.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(req.body);
+    const newUser = new User({ email,username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to WonderHub!");
+    res.redirect("/listings");
+});
+      
 
 module.exports = router;
